1d5e461d520d8350bcffc59b0a113ea911ee4b1c
s30291-pj <s30291@pjwstk.edu.pl>
2025-01-11 00:35:48 +0100

    micro pataches

--- DATA_CONSISTANCY_QUERY.sql
+++ DATA_CONSISTANCY_QUERY.sql
@@ -1,68 +1,58 @@
-# Kwerenda sprawdzająca spójność danych 
-# (dane zostaly specjalnie dodane podczas tworzenia, aby mogly byc wyswietlone jako niespojne)
-
-# Sprawdzenie spojnosci adresow
-SELECT COUNT(adr.id) AS nieprzypisane_adresy FROM 
-(
-	SELECT adres_id FROM lekarz
+# Wyswietlenie nieprzypisanych adresow
+SELECT CONCAT("NIE PRZYPISANO NIKOMU ADRESU O ID ", id) AS informacja FROM adres adr
+LEFT OUTER JOIN (
+	SELECT adres_id FROM pacjent
     UNION
-    SELECT adres_id FROM pacjent
-) AS p_adr
-RIGHT OUTER JOIN adres AS adr
+    SELECT adres_id FROM lekarz
+) AS p_adr 
 ON p_adr.adres_id = adr.id
-WHERE p_adr.adres_id is null
-;
+WHERE adres_id IS NULL
 
-# Wyswietelenie niespojnych adresow
-SELECT * FROM 
-(
-	SELECT adres_id FROM lekarz
+UNION
+
+# Wyswietlenie pacjentow lub lekarzy z nieprzypisanym adresem
+SELECT CONCAT("NIE PRZYPISANO OSOBIE ", osoba, " O ID ", id_osoby, " ADRESU!") AS informacja FROM adres adr
+RIGHT OUTER JOIN (
+    SELECT adres_id, id AS id_osoby, osoba FROM (SELECT id, adres_id, "PACJENT" AS osoba FROM pacjent) AS pacjenci
     UNION
-    SELECT adres_id FROM pacjent
-) AS p_adr
-RIGHT OUTER JOIN adres
-ON p_adr.adres_id = adres.id
-WHERE p_adr.adres_id is null
-;
+    SELECT adres_id, id AS id_osoby, osoba FROM (SELECT id, adres_id, "PACJENT" AS osoba FROM lekarz) AS lekarze
+) AS p_adr 
+ON p_adr.adres_id = adr.id
+WHERE id IS NULL
 
-SELECT * FROM 
-(
-	SELECT * FROM 
-	(
-		SELECT adres_id FROM lekarz
-		UNION
-		SELECT adres_id FROM pacjent
-	) AS p_adr
-	LEFT JOIN adres
-	ON p_adr.adres_id = adres.id
-	UNION ALL
-	SELECT * FROM 
-	(
-		SELECT adres_id FROM lekarz
-		UNION
-		SELECT adres_id FROM pacjent
-	) AS p_adr
-	RIGHT JOIN adres
-	ON p_adr.adres_id = adres.id
-) as dane
-WHERE dane.adres_id is null OR dane.id is null;
+UNION
 
+# Wyswietlanie diagnozy bez przypisania
+SELECT CONCAT("DIAGNOZA O ID ", diagnoza.id, " NIE JEST PRZYPISANA DO ZADNEJ WIZYTY!") AS informacja FROM diagnoza 
+LEFT OUTER JOIN wizyta
+ON diagnoza.id = wizyta.diagnoza_id
+WHERE wizyta.id IS NULL
 
-# Sprawdzenie spojnosci diagnoz
-SELECT COUNT(dgz.id) AS nieprzypisane_diagnozy FROM diagnoza AS dgz 
-LEFT OUTER JOIN wizyta AS wiz 
-ON dgz.id = wiz.diagnoza_id 
-WHERE wiz.id is null;
+UNION
 
-# Wyswietelenie niespojnych diagnoz
-SELECT * FROM diagnoza AS dgz 
-LEFT OUTER JOIN wizyta AS wiz 
-ON dgz.id = wiz.diagnoza_id 
-WHERE wiz.id is null;
+# Wyswietlanie niespojnosci w tabeli przypisane_leczenie
+SELECT CONCAT("LECZENIE DLA DIAGNOZY O ID ", p_le.diagnoza_id, " NIE ISTNIEJE!") AS informacja 
+FROM przypisane_leczenie p_le
+LEFT OUTER JOIN leczenie le 
+ON p_le.leczenie_id = le.id
+WHERE le.id IS NULL
 
-# Sprawdzanie spojnosci leczen
-SELECT COUNT(diagnoza_id) AS niespojne_przypisane_leczenia FROM przypisane_leczenie AS p_le 
-LEFT OUTER JOIN diagnoza AS dgz ON p_le.diagnoza_id = dgz.id WHERE dgz.id is NULL;
+UNION
+
+# Wyswietlanie niespojnosci w tabeli przypisane_specjalizacje
+SELECT CONCAT("SPECJALIZACJA DLA LEKARZA O ID ", p_sp.lekarz_id, " NIE ISTNIEJE!") AS informacja 
+FROM przypisana_specjalizacja p_sp
+LEFT OUTER JOIN specjalizacja sp ON p_sp.specjalizacja_id = sp.id
+WHERE sp.id IS NULL
+
+UNION
+
+# Wyswietlanie niespojnosci w tabeli przypisany_oddzial
+SELECT CONCAT("ODDZIAL DLA LEKARZA O ID ", p_od.lekarz_id, " NIE ISTNIEJE!") AS informacja 
+FROM przypisany_oddzial p_od
+LEFT OUTER JOIN oddzial od ON p_od.oddzial_id = od.id
+WHERE od.id IS NULL;
+
 
 
 
